c7ed5e548f76baa805ea1a77cb91f9552abb71a4
sekR4 <sebastian.kraus.jena@gmail.com>
2020-04-20 20:22:20 +0200

    Frustrating experiences with HTMLParser :)

--- Hackerrank/Python/html-parser-part-1.py
+++ Hackerrank/Python/html-parser-part-1.py
@@ -1,29 +1,33 @@
-# from parser import HTMLParser
-# import parser
+# https://docs.python.org/3.8/library/html.parser.html?highlight=htmlparser#html.parser.HTMLParser
+
+# n = 2
+# samples = ["<html><head><title>HTML Parser - I</title></head>",
+#            "<body data-modal-target class='1'><h1>HackerRank</h1><br /></body></html>"]
+
 from html.parser import HTMLParser
-
-n = 2
-samples = ["<html><head><title>HTML Parser - I</title></head>",
-           "<body data-modal-target class='1'><h1>HackerRank</h1><br /></body></html>"]
-
-# for i in range(n):
-#     print(samples[i])
 
 
 class MyHTMLParser(HTMLParser):
+    # qingchen's solution
     def handle_starttag(self, tag, attrs):
-        print("Start :", tag)
+        print('Start :', tag)
+        for ele in attrs:
+            print('->', ele[0], '>', ele[1])
 
     def handle_endtag(self, tag):
-        print("End :", tag)
+        print('End   :', tag)
 
-    def handle_data(self, data):
-        print("Encountered some data  :", data)
+    def handle_startendtag(self, tag, attrs):
+        print('Empty :', tag)
+        for ele in attrs:
+            print('->', ele[0], '>', ele[1])
 
 
 parser = MyHTMLParser()
 
-code = ''
-for sample in samples:
-    code += sample
-parser.feed(code)
+for i in range(int(input())):
+    parser.feed(input())
+
+
+# for sample in samples:
+#     parser.feed(sample)
