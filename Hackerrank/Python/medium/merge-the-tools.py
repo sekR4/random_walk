8bc20fe47959fe5f8c73ad57452050017af01e53
sekR4 <sebastian.kraus.jena@gmail.com>
2020-05-02 21:10:43 +0200

    played a bit with an R-logistic Regression

--- Hackerrank/Python/medium/merge-the-tools.py
+++ Hackerrank/Python/medium/merge-the-tools.py
@@ -1,23 +1,53 @@
+import os
 import collections
-s = 'AABCAAADA'
-k = 3
-
-t_length = len(s)//k
-
-# print(t_length)
-for i in range(k):
-    # print(i)
-    # print(s[i*t_length:t_length+i*t_length])
-    # print(set(s[i*t_length:t_length+i*t_length]))
-    print(''.join(collections.OrderedDict.fromkeys(s[i*t_length:t_length+i*t_length]).keys()))
-# print(s[3])
+# s = 'AABCAAADA'
+# k = 3
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # set working directory
+with open('merge-the-tools-test-case-1-input.txt', 'r') as file:
+    inp = file.read()
+    s, k = inp.split('\n')[0], int(inp.split('\n')[1])
 
 
+# tmp, len_tmp = [], 0
+
+# for i in s:
+#     len_tmp += 1
+#     if i not in tmp:
+#         tmp.append(i)
+#     if len_tmp == k:
+#         print(''.join(tmp))
+#         tmp, len_tmp = [], 0
+
+# tmp, len_tmp = '', 0
+# for letter in s:
+#     len_tmp += 1
+#     if letter not in tmp:
+#         tmp += letter
+#     if len_tmp == k:
+#         print(tmp)
+#         tmp, len_tmp = '', 0
+
+
+# tmp = ''
+# # print(len(tmp))
+# for letter in s:
+#     if letter not in tmp:
+#         tmp += letter
+#     if len(tmp)-1 == k:
+#         print(tmp)
+#         tmp = ''
+# inspired by lesingerouge
 def merge_the_tools(string, k):
-    t_length = len(string)//k
-    for i in range(k):
-        print(''.join(collections.OrderedDict.fromkeys(
-            string[i*t_length:t_length+i*t_length]).keys()))
+    tmp, len_tmp = '', 0
+    for letter in string:
+        len_tmp += 1
+        if letter not in tmp:
+            tmp += letter
+        if len_tmp == k:
+            print(tmp)
+            tmp, len_tmp = '', 0
 
 
+#
+#
 merge_the_tools(s, k)
