7e696197c37434523f99c1e21e8acf1b9b1b4e23
sekR4 <sebastian.kraus.jena@gmail.com>
2020-06-02 08:28:05 +0200

    added setup atom

---
+++ setting_up_atom.sh
@@ -0,0 +1,13 @@
+# Atom Packeges & Themes
+apm install script                # Makes scripts executable within atom.
+apm install predawn-syntax        # Adjust settings then. It looks nice.
+apm install autocomplete-python   # Get some python specific suggestions during coding.
+apm install file-icons            # Just helpful :)
+apm install minimap               # Just helpful :)
+apm install python-autopep8       # Automatically adjusts your code to PEP8 when saving. (Check in settings first)
+apm install linter-flake8         # Shows you whats wrong
+apm install linter                # Dependency for flake8
+
+# Python Packeges
+pip install autopep8
+pip install flake8
