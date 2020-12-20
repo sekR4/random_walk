3c382b1eb4cb9aa4577347ac0f7df67423b30142
sekR4 <sebastian.kraus.jena@gmail.com>
2020-12-20 08:02:22 +0100

    problem solved :)

--- Hackerrank/Java/java-if-else.java
+++ Hackerrank/Java/java-if-else.java
@@ -8,16 +8,25 @@ import java.util.regex.*;
 // Wahnsinn, was ich hier alles importieren muss für ein so banales Problem...
 public class Solution {
 
-
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // TODO: 14.12.20 Baue den Rest der Task auch mit ein, denn 18 soll "Weird" sein. ^^ 
+        // https://www.hackerrank.com/challenges/java-if-else/problem
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        if(N % 2==0) { System.out.println("Not Weird"); }
-        else { System.out.println("Weird"); }
+        if (N % 2 == 0) {
+            if (N > 1 && N < 5) {
+                System.out.println("Not Weird");
+            }
+            if (N > 5 && N < 21) {
+                System.out.println("Weird");
+            }
+            if (N > 20) {
+                System.out.println("Not Weird");
+            }
+        }
+        else System.out.println("Weird");
+
         scanner.close();
     }
 }
